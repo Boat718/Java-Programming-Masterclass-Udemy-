7f5aede32006eaab82e7b39df6cb923900f4def3
Boat <Boat_loading@hotmail.com>
2022-05-17 01:25:14 +0700

    Done

---
+++ Section6  OOP/Exercise5/src/Floor.java
@@ -0,0 +1,24 @@
+public class Floor {
+
+    private double width, length;
+
+    public Floor(double width, double length) {
+        if(width < 0) {
+            this.width = 0;
+        }
+        if(length < 0){
+            this.length = 0;
+        }
+        if(width > 0) {
+            this.width = width;
+        }
+        if(length > 0){
+            this.length = length;
+        }
+    }
+
+    public double getArea(){
+        return this.length * this.width;
+    }
+
+}
