8d78ee0f3691deddee5440715dcaa1865fe6f188
Boat <Boat_loading@hotmail.com>
2022-05-28 22:44:31 +0700

    Done

---
+++ Section6  OOP/Polymorphism/src/Car.java
@@ -0,0 +1,27 @@
+public class Car {
+    private boolean engine;
+    private int cylinders, wheels;
+    private String name;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.engine = true;
+        this.wheels = 4;
+    }
+
+    public String startEngine() {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> break()";
+    }
+
+
+
+}
